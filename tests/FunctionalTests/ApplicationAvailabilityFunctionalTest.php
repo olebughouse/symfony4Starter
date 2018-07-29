170eda19a867fa26da04f2b869d82dd877ddccd2
Oliver Backhaus <olli.backhaus@googlemail.com>
2018-07-29 11:22:16 +0200

    Added basic functional and unit test

---
+++ tests/FunctionalTests/ApplicationAvailabilityFunctionalTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApplicationAvailabilityFunctionalTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function urlProvider()
+    {
+        //yield ['/'];
+        yield ['/lucky/number'];
+        yield ['/tiles/{slug}'];
+        // ...
+    }
+}
